e04840f8946e59713a20240eae3f867ecfab9644
ItsOfficiallyMe <mr.aiofkingdom@gmail.com>
2025-07-12 13:17:46 +0530

    Create time_series_visualizer.py

---
+++ time_series_visualizer.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Load data
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
+
+def draw_line_plot():
+    pass
+
+def draw_bar_plot():
+    pass
+
+def draw_box_plot():
+    pass
